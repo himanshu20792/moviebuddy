6114e024a950372a098f9a8292412ab9bb6d619a
Alan Wang <alanwang@Alans-MacBook-Air.local>
2020-07-22 23:57:27 -0400

    Added templates

--- recommender/views.py
+++ recommender/views.py
@@ -4,10 +4,9 @@ def home(request):
     return render(request, 'recommender/home.html')
 
 def wordcloud(request):
-    return HttpResponse("You're looking at question")
-    #return render(request, 'recommender/wordcloud.html')
+    return render(request, 'recommender/wordcloud.html')
 
 def movieselection(request):
     return render(request, 'recommender/movieselection.html')
 
-# Create your views here.
+
