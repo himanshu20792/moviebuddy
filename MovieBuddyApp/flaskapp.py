15cab8114248695d4a3e74bd42e6d34afa4526ae
Himanshu <himanshu.agarwal20792@gmail.com>
2020-08-29 02:05:45 +0530

    Homepage improvement
    Added functionality to findsimilar button, changed position of the text from pixel to percentage

--- MovieBuddyApp/flaskapp.py
+++ MovieBuddyApp/flaskapp.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/findsimilar")
+def findsimilar():
+    return render_template('findsimilar.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
