85af1a2bc5199b22fe6b87aad163ae72571bdcff
hilsdsg3 <schnettler.ty@gmail.com>
2020-08-28 21:11:54 -0700

    Added for the app a wordcloud page

--- MovieBuddyApp/flaskapp.py
+++ MovieBuddyApp/flaskapp.py
@@ -10,5 +10,9 @@ def home():
 def findsimilar():
     return render_template('findsimilar.html')
 
+@app.route("/wordcloud")
+def findsimilar():
+    return render_template('wordcloud.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
