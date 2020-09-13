810404f07298e35b1ab7670b9c5f573f10d56b1e
hilsdsg3 <schnettler.ty@gmail.com>
2020-09-13 00:45:48 -0700

    Added a simple dB

--- MovieBuddyApp/flaskapp.py
+++ MovieBuddyApp/flaskapp.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://search.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/champ/Python_proj/made_with_ml_repo/temp/moviebuddy/MovieBuddyApp/search.db'
+# Initialize the database
+db = SQLAlchemy(app)
+
+# Create db model
+class search(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    search_term = db.Column(db.String(200), nullable=False)
+    wc_drop_down_name = db.Column(db.String(200), nullable=False)
+    wc_picture_type = db.Column(db.String(200), nullable=False)
+
+    # Create a funtion to return a string when we add something
+    def __repr__(self):
+        return '<wc_picture_type %r>' % self.search_term 
+    
 @app.route("/")
 @app.route("/home")
 def home():
